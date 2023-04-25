049befebe45d103522b5a6eed19a6995261a28a2
bahaasala <salaymeh1999@live.com>
2023-04-25 17:48:33 +0200

    added views and routes

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
+// lisetning to port
+app.listen(port, (err) => {
+    if (err) console.log(err);
     console.log(`Example app listening on port ${port}`)
 })
 
+// serving static files in Express
+app.use(express.static('public'))
+
+// setting views to views folder
+app.set('view engine', 'ejs')
+    .set('views', 'views')
+
+// routes
 app
-    .get('/', onHome)
-    .get('/users/:userId/books/:bookId', onUser)
-
-
-function onHome(req, res) {
-    res.send('root')
-}
-
-function onUser(req, res) {
-    res.send(req.params)
-}
+    .get('/', (req, res) => {
+        res.render('index.ejs')
+    })
+    .get('/users/:user', (req, res) => {
+        const user = req.params.user;
+        res.render('profile.ejs', {
+            user: user
+        });
+    })
