e65676ffe47e395a6de4ad6fd7e19d5d72db00cd
bahaasala <salaymeh1999@live.com>
2023-05-11 22:10:23 +0200

    html tags in ejs files

--- connect.js
+++ connect.js
@@ -1,28 +1,38 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require('dotenv').config();
 
-async function main() {
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_NAME}.dzdhppv.mongodb.net/?retryWrites=true&w=majority`;
+// Replace the placeholder with your Atlas connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.CLUSER_URL}.dzdhppv.mongodb.net/?retryWrites=true&w=majority`;
 
-    const client = new MongoClient(uri);
-
-    try {
-        await client.connect();
-    
-        await listDatabases(client);
-     
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
     }
-}
-main().catch(console.error);
+);
 
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+async function run() {
+    try {
+      await client.connect();
+
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+      // Select the sample_airbnb database
+      const db = client.db('sample_airbnb');
+  
+      // List all collections in the sample_airbnb database
+      const collections = await db.listCollections().toArray();
+      console.log('Collections:', collections.map(c => c.name));
+    } catch (err) {
+        console.log(err);
+    } finally {
+      await client.close();
+    }
+  }
+  
+  run().catch(console.dir);
