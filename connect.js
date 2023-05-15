4b688141fbab9345de229e0cd4b69ec944253e20
bahaasala <salaymeh1999@live.com>
2023-05-15 22:28:36 +0200

    added dynamic data

--- connect.js
+++ connect.js
@@ -1,38 +1,30 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
-require('dotenv').config();
+require("dotenv").config();
 
-// Replace the placeholder with your Atlas connection string
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.CLUSER_URL}.dzdhppv.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-async function run() {
-    try {
-      await client.connect();
+const db = client.db(process.env.DB_NAME);
 
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  
-      // Select the sample_airbnb database
-      const db = client.db('sample_airbnb');
-  
-      // List all collections in the sample_airbnb database
-      const collections = await db.listCollections().toArray();
-      console.log('Collections:', collections.map(c => c.name));
-    } catch (err) {
-        console.log(err);
-    } finally {
-      await client.close();
-    }
+async function connect() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
-  
-  run().catch(console.dir);
+}
+
+module.exports = { db, connect };
--- server.js
+++ server.js
@@ -1,50 +1,96 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const multer = require("multer");
+const { db, connect } = require("./connect.js");
+const app = express();
+const port = 3000;
 
-// lisetning to port
-app.listen(port, (err) => {
-    if (err) console.log(err);
-    console.log(`Example app listening on port ${port}`)
-})
-
-// connect database
-const connect = require('./connect');
+// connect to MongoDB
+connect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
 
 // serving static files in Express
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // setting views to views folder
-app.set('view engine', 'ejs')
-    .set('views', 'views')
+app.set("view engine", "ejs").set("views", "views");
 
 // urlencoded for form data
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-// routes
+// Configure multer to specify where to save uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    const fileName = Date.now() + "-" + file.originalname;
+    cb(null, fileName);
+  },
+});
+
+// file upload
+const upload = multer({ storage });
+
+// routes GET requests
 app
-    .get('/', (req, res) => {
-        res.render('index.ejs')
-    })
-    .get('/trips', (req, res) => {
-        res.render('trips.ejs')
-    })
-    .get('/trips/:trip', (req, res) => {
-        res.render('trip_details.ejs')
-    })
-    .get('/trips/:trip/book', (req, res) => {
-        res.render('book.ejs')
-    })
-    .get('/trips/:trip/book/:bookingNR/confirmed', (req, res) => {
-        res.render('confirmed.ejs')
-    })
-    .get('/users/:user', (req, res) => {
-        const user = req.params.user;
-        res.render('profile.ejs', {
-            user: user
-        });
-    })
-    // 404 page
-    // .use((req, res) => {
-    //     res.status(404).render('not_found.ejs');
-    // });
+  .get("/", async (req, res, next) => {
+    try {
+      const trips = await db.collection("trips").find().toArray();
+      res.render("index.ejs", { trips: trips });
+    } catch (err) {
+      next(err);
+    }
+  })
+  .get("/trips", async (req, res, next) => {
+    try {
+      const user = await db.collection("users").findOne({ name: "Bahaa" });
+      const trips = await db.collection("trips").find().toArray();
+      console.log(trips);
+      res.render("trips.ejs", { user: user, trips: trips });
+    } catch (err) {
+      next(err);
+    }
+  })
+  .get("/trips/:trip", (req, res) => {
+    res.render("trip_details.ejs");
+  })
+  .get("/trips/:trip/book", (req, res) => {
+    res.render("book.ejs");
+  })
+  .get("/trips/:trip/book/:bookingNR/confirmed", (req, res) => {
+    res.render("confirmed.ejs");
+  })
+
+  .get("/users/:user", (req, res) => {
+    const user = req.params.user;
+    res.render("profile.ejs", {
+      user: user,
+    });
+  });
+
+// routes POST requests
+app.post("/trips", upload.single("image"), async (req, res, next) => {
+  try {
+    const { destination } = req.body;
+    const imagePath = req.file.filename;
+
+    // Insert the new trip into the MongoDB collection
+    await db.collection("trips").insertOne({ destination, image: imagePath });
+
+    res.redirect("/trips"); // Redirect to the trips page after successful submission
+  } catch (err) {
+    next(err);
+  }
+});
+
+// 404 page
+// .use((req, res) => {
+//     res.status(404).render('not_found.ejs');
+// });
